ea34849990c6d316e3719dac17c02d9e792c9678
Abhi-xek <146823773+Abhi-xek@users.noreply.github.com>
2024-02-12 17:40:46 +0530

    Time: 7 ms (69.25%), Space: 15.6 MB (27.71%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int operation(int a, int b, string i){
-        if(i == "+") return a+b;
-        if(i == "-") return b-a;
-        if(i == "*") return long(a)*long(b);
-        if(i == "/") return b/a;
+    // int operation(int a, int b, string i){
+    //     if(i == "+") return a+b;
+    //     if(i == "-") return b-a;
+    //     if(i == "*") return long(a)*long(b);
+    //     if(i == "/") return b/a;
 
-        return -1;
-    }
+    //     return -1;
+    // }
 
     int evalRPN(vector<string>& tokens) {
         int result = 0;
         stack<int>st;
+
+        unordered_map<string, function<int(int, int)>>m{
+            {"+", [](int a , int b){return a+b;}}, 
+            {"-", [](int a, int b){return b-a;}},
+            {"*", [](int a, int b){return long(a)*long(b);}},
+            {"/", [](int a, int b){return b/a;}}
+        };
+
+        
 
         for(auto i : tokens){
             if(i =="+" ||i =="-" ||i == "*" ||i == "/" ){
@@ -19,7 +28,7 @@ public:
                 st.pop();
                 int b = st.top();
                 st.pop();
-                st.push(operation(a, b, i));
+                st.push(m[i](a,b));
             }
             else{
                 st.push(stoi(i));
