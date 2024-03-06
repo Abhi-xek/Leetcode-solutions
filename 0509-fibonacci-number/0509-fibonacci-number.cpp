68aae5cff89f43c2c933d90486a0b069f3764fa5
Abhi-xek <146823773+Abhi-xek@users.noreply.github.com>
2024-03-06 22:14:06 +0530

    Time: 1 ms (78.8%), Space: 7.3 MB (9.3%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0) return 0;
+
+        if(n == 0) return 0;
         if(n == 1) return 1;
-        int prev1 = 0; 
-        int prev2 = 1; 
-        int current = 0;
+       vector<int> dp(n+1, 0);
+     
+     dp[0] = 0;
+     dp[1] = 1; 
 
-        for(int i = 2; i<=n; i++){
-            current = prev1 + prev2;
-            prev1 = prev2; 
-            prev2 = current;
-        }
+     for(int i = 2; i<=n; i++){
+         dp[i] = dp[i-1] + dp[i-2];
+     }
 
-        return current;
+     return dp[n];
     }
 };
